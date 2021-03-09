898626a707f88d3838d800e39cbf055d08bc914f
gustavodaltoe <gustavodaltoe@hotmail.com>
2021-03-09 03:01:45 -0300

    :wastebasket: deleção de testes nao implementados

--- src/components/Header/test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import Header from '.';
-
-describe('<Header />', () => {
-  it('should describe here', () => {
-    const { container } = render(<Header />);
-  });
-});
